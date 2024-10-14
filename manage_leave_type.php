6dc11fc5f7bb7494b1f6f7b5556b1fea19f1d769
beckpalmx@gmail.com <beckpalmx@gmail.com>
2024-10-14 17:27:38 +0700

    Update manage_leave_type.php

--- manage_leave_type.php
+++ manage_leave_type.php
@@ -290,7 +290,7 @@ if (strlen($_SESSION['alogin']) == "" || strlen($_SESSION['department_id']) == "
         $(document).ready(function () {
             let formData = {action: "GET_LEAVE_TYPE", sub_action: "GET_MASTER"};
             let dataRecords = $('#TableRecordList').DataTable({
-                'lengthMenu': [[10, 20, 50, 100], [10, 20, 50, 100]],
+                'lengthMenu': [[7 ,10, 20, 50, 100], [7 ,10, 20, 50, 100]],
                 'language': {
                     search: 'ค้นหา', lengthMenu: 'แสดง _MENU_ รายการ',
                     info: 'หน้าที่ _PAGE_ จาก _PAGES_',
