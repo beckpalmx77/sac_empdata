88c7dc2eee0a2da2ff23fd70ba3df538b12119f9
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-03-19 17:58:38 +0700

    Update Dashboard_employee_smart.php

--- Dashboard_employee_smart.php
+++ Dashboard_employee_smart.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="th">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Icon Navigation</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link href="../img/logo/logo.png" rel="icon">
+    <title>สงวนออโต้คาร์ | SANGUAN AUTO CAR</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <style>
         html, body {
