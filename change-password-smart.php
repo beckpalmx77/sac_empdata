f538d446a4caadc8aec00a45272b7a83f9660b65
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-03-20 14:13:26 +0700

    Update change-password-smart.php

--- change-password-smart.php
+++ change-password-smart.php
@@ -24,13 +24,13 @@ if (strlen($_SESSION['alogin']) == "" || strlen($_SESSION['department_id']) == "
                 <div class="container-fluid" id="container-wrapper">
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800"><?php echo urldecode($_GET['s']) ?></h1>
-                        <ol class="breadcrumb">
+                        <!--ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="<?php echo $_SESSION['dashboard_page'] ?>">Home</a>
                             </li>
                             <li class="breadcrumb-item"><?php echo urldecode($_GET['m']) ?></li>
                             <li class="breadcrumb-item active"
                                 aria-current="page"><?php echo urldecode($_GET['s']) ?></li>
-                        </ol>
+                        </ol-->
                     </div>
 
                     <div class="row">
