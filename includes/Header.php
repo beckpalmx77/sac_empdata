52ae7886ba3f7d47df03a26efbb8b5c3b5709b3a
beckpalmx77 <beckpalmx@gmail.com>
2025-08-13 10:43:31 +0700

    Update Header.php

--- includes/Header.php
+++ includes/Header.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     <link href="../img/logo/logo.png" rel="icon">
 
     <title>สงวนออโต้คาร์ | SANGUAN AUTO CAR</title>
