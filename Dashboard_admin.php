3fcda79e71a62f1ab1126f926bb39bf4e718829b
beckpalmx77 <beckpalmx@gmail.com>
2025-10-24 17:11:35 +0700

    Update Dashboard_admin.php

--- Dashboard_admin.php
+++ Dashboard_admin.php
@@ -24,6 +24,9 @@ if (strlen($_SESSION['alogin']) == "") {
                 include('includes/Top-Bar.php');
                 ?>
                 <div class="container-fluid" id="container-wrapper">
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800"><?php echo "เอกสารการลา/ใช้วันหยุดประจำปี รายวัน" ?></h1>
+                    </div>
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="card mb-12">
